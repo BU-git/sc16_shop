07fa768629c7fe87c2432f96ce3d55ccf4c16119
MikaBondarenko <misha101090@gmail.com>
2016-07-19 23:13:38 +0300

    add files

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var cssmin = require('gulp-cssmin');
+var uglify = require('gulp-uglify');
+
+gulp.task('cssConcat', function(){
+	return gulp.src([
+		'./src/css/reset.css',
+		'./src/css/style.css'
+		])
+    .pipe(concat('main.css'))
+    .pipe(cssmin())
+    .pipe(gulp.dest('./build/css'));
+});
+gulp.task('jsConcat', function() {
+  return gulp.src(['./src/js/script.js','./src/js/slider.js'])
+    .pipe(concat('main.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js'));
+});
+gulp.task('build', ['jsConcat','cssConcat']);
+gulp.task('watch',function(){
+    gulp.watch('./src/css/*.css', ['cssConcat']);
+    gulp.watch('./src/js/*.js', ['jsConcat']);
+});
+gulp.task('default',['build','watch']);
