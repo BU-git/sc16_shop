4606c52388a4ef6c12e9139028739f6d0476d107
pevac <home.peva@gmail.com>
2016-07-22 12:36:21 +0300

    refactored validator phone

--- src/js/validate.js
+++ src/js/validate.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
 
 jQuery.validator.addMethod('phoneUS', function(phone_number, element) {
     phone_number = phone_number.replace(/\s+/g, ''); 
-    return this.optional(element) || phone_number.length > 9 &&
-        phone_number.match(/^(1-?)?(\([2-9]\d{2}\)|[2-9]\d{2})-?[2-9]\d{2}-?\d{4}$/);
+    return this.optional(element) || phone_number.length > 3 &&
+        phone_number.match(/^\d[\d\(\)\ -]{4,14}\d$/);
 }, 'Please enter a valid phone number.');
 
 
