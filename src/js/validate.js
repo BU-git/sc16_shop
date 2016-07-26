59a78a05001d018f1d4ed6a62bb9fe4aa8a282e2
c1313 <c1313@bionic.local>
2016-07-26 20:44:29 +0300

    Fixed header

---
+++ src/js/validate.js
@@ -0,0 +1,80 @@
+$(document).ready(function(){
+
+  jQuery.validator.addMethod('phoneUS', function(phone_number, element) {
+    phone_number = phone_number.replace(/\s+/g, ''); 
+    return this.optional(element) || phone_number.length > 5 &&
+    phone_number.match(/^\d[\d\(\)\ -]{3,14}\d$/);
+  }, "Please specify a valid  phone number");
+
+
+
+  jQuery.validator.addMethod( "lettersonly", function( value, element ) {
+  return this.optional( element ) || /^[а, б, в, г, ґ, д, е, є, ж, з, и, і, ї, й, к, л, м, н, о, п, р, с, т, у,', ф, х, ц, ч, ш, щ, ь, ю, я,ъ,ё,ы\-\a-z]+$/i.test( value );
+}, "Letters or punctuation only please" );
+
+  $("#formValidate").validate({
+   rules:{
+    first_name: {
+      required: true,
+      maxlength: 20,
+      lettersonly: true
+    },
+
+    last_name: {
+      required: true,
+      maxlength: 20,
+      lettersonly: true
+    },
+
+    tel:{
+      required: true,
+      phoneUS: true,
+    },
+
+    email:{
+      email: true,
+      required: false,
+    },
+
+    textarea:{
+      required: false,
+      maxlength: 100,
+    }
+  },
+
+  messages:{
+    first_name:{
+      required: "Введите ваше имя.",
+      maxlength: $.validator.format( "Количество символов должно быть небольше {0}." ),
+      lettersonly: "Введите только букви."
+    },
+
+    last_name:{
+      required: "Введите вашу фамилию",
+      maxlength: $.validator.format( "Количество символов должно быть небольше {0}." ),
+       lettersonly: "Введите только букви."
+    },
+
+    tel:{
+      required: "Введите ваш телефон.",
+      phoneUS: "Введите корректный номер телефона"
+    },
+
+    email:{ email:"Введите корректный  email."},
+
+    textarea:{  maxlength: $.validator.format( "Количество символов должно быть небольше {0}." )}
+  },
+
+  errorElement : 'div',
+  errorPlacement: function(error, element) {
+    var placement = $(element).data('error');
+    if (placement) {
+      $(placement).append(error)
+    } else {
+      error.insertAfter(element).data('error');
+    }
+  },
+
+});
+});
+
