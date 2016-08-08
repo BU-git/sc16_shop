7cae37cac3ea405f3e42bc74135923c1d32f9599
c1315 <c1315@bionic.local>
2016-08-08 19:47:19 +0300

    fixed landing

--- src/js/script.js
+++ src/js/script.js
@@ -3,12 +3,12 @@
 		$('.materialboxed').materialbox();
 		$(".button-collapse").sideNav();
 		$('select').material_select();
-		$("#menu").on("click","a[href*=#]:not([href=#])", function (event) {
-			event.preventDefault();
-			var id  = $(this).attr('href'),
-			top = $(id).offset().top;
-			$('body,html').animate({scrollTop: top}, 1500);
-		});
+		// $("#menu").on("click","a[href*=#]:not([href=#])", function (event) {
+		// 	event.preventDefault();
+		// 	var id  = $(this).attr('href'),
+		// 	top = $(id).offset().top;
+		// 	$('body,html').animate({scrollTop: top}, 1500);
+		// });
 		$('.slider').slider({indicators: false, height: 300, full_width: true});
 		$('#next-slider').click(function(){ 
 			$('.slider').slider('next');
