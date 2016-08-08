65cccc11e32f4583691850f01c96f5d7d1452364
pevac <home.peva@gmail.com>
2016-08-08 14:26:32 +0300

    fixed header

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+	$(document).ready(function(){
+		$('ul.tabs').tabs();
+		$('.materialboxed').materialbox();
+		$(".button-collapse").sideNav();
+		$('select').material_select();
+		$("#menu").on("click","a[href*=#]:not([href=#])", function (event) {
+			event.preventDefault();
+			var id  = $(this).attr('href'),
+			top = $(id).offset().top;
+			$('body,html').animate({scrollTop: top}, 1500);
+		});
+		$('.slider').slider({indicators: false, height: 300, full_width: true});
+		$('#next-slider').click(function(){ 
+			$('.slider').slider('next');
+		}); 
+		$('#prev-slider').click(function(){ 
+			$('.slider').slider('prev');
+		}); 
+	});
