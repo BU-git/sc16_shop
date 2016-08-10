56663ea19d7a0c2f0fbac255c4d34754551c1974
pevac <home.peva@gmail.com>
2016-08-10 11:33:37 +0300

    merge project

--- gulpfile.js
+++ gulpfile.js
@@ -6,32 +6,31 @@ var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
 
 gulp.task('cssConcat', function(){
-    return gulp.src(['./src/css/*.css'])
-    .pipe(concat('main.css'))
-    .pipe(cssmin())
-    .pipe(gulp.dest('./build/css'));
+    return gulp.src(['./src/css/*.css'])
+   .pipe(concat('main.css'))
+   .pipe(cssmin())
+   .pipe(gulp.dest('./build/css'));
 });
 gulp.task('image:build', function(){
-    return gulp.src('./src/images/**/*.*')
-        .pipe(imagemin({ 
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            use: [pngquant()],
-            interlaced: true
-        }))
-        .pipe(gulp.dest('./build/images'))
-        .pipe(reload({stream: true}));
+    return gulp.src('./src/images/**/*.*')
+       .pipe(imagemin({ 
+           progressive: true,
+           svgoPlugins: [{removeViewBox: false}],
+           use: [pngquant()],
+           interlaced: true
+       }))
+       .pipe(gulp.dest('./build/images'));
 });
 gulp.task('jsConcat', function() {
-  return gulp.src(['./src/js/*.js'])
-    .pipe(concat('main.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('./build/js'));
+ return gulp.src(['./src/js/*.js'])
+   .pipe(concat('main.js'))
+   .pipe(uglify())
+   .pipe(gulp.dest('./build/js'));
 });
 gulp.task('build', ['jsConcat','cssConcat','image:build']);
 gulp.task('watch',function(){
-    gulp.watch('./src/css/*.css', ['cssConcat']);
-    gulp.watch('./src/js/*.js', ['jsConcat']);
-    gulp.watch('./src/images/**/*.*', ['image:build']);
+   gulp.watch('./src/css/*.css', ['cssConcat']);
+   gulp.watch('./src/js/*.js', ['jsConcat']);
+    gulp.watch('./src/images/**/*.*', ['image:build']);
 });
 gulp.task('default',['build','watch']);
